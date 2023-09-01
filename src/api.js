0d24e6d4bcf06d10e3f421a03ae7ceaca65f3696
Indira Borras Gonzalez <ibg.indira@gmail.com>
2023-09-01 13:00:05 +0100

    fetch for cities complete

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '068ed59d2bmsh0cca71171a3d314p129074jsn92ba3b1bd837',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+
+
+
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,10 +1,30 @@
 import React from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 import { useState } from "react";
+import { GEO_API_URL, geoApiOptions } from "../../api";
 
-const Search = ({onSearchChange}) => {
+const Search = ({ onSearchChange }) => {
 
-    const [search, setSearch] = useState("")
+    const [search, setSearch] = useState(null)
+
+    const loadOptions = (inputValue) => {
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+            geoApiOptions // the options for the fetch method
+        )
+            .then(response => response.json())
+            .then(response => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`,
+                        }
+                    })
+                }
+            })
+            .catch(err => console.error(err))
+    }
 
     const handleOnChange = (searchData) => {
         setSearch(searchData);
@@ -14,14 +34,24 @@ const Search = ({onSearchChange}) => {
 
 
     return (
-        <AsyncPaginate 
-        placeholder='Search for City'
-        debounceTimeout={600}
-        value={search}
-        onChange={handleOnChange}
+        <AsyncPaginate
+            placeholder='Search for City'
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
         />
     )
 
 }
 
 export default Search;
+
+ //fetching with async. Same result as fetch + .then + .catch, but looks more synchronous
+    // try {
+    //      const response = await fetch(url, options);
+    //      const result = await response.text();
+    //      console.log(result);
+    // } catch (error) {
+    //      console.error(error);
+    // }
